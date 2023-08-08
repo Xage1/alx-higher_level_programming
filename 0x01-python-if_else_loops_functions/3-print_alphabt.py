d2feda1c240021319b5b46e2bdf42413ca13e337
Xage1 <kennethkiarie555@gmail.com>
2023-08-08 11:10:49 -0700

    More Python what I want

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for char in range(ord('a'), ord('z') + 1):
-    if chr(char) != 'q' and chr(char) != 'e':
-        print(chr(char), end="")
+for n in range (0, 26):
+    if n != 4 and n != 16:
+        print(f"{chr(ord('a') + n)}", end="")
