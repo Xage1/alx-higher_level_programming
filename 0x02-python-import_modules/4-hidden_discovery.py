8ece83547214edeb8666cb99516b2d5422de661c
Xage1 <kennethkiarie555@gmail.com>
2023-08-11 03:39:34 -0700

    Python New Project

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,17 +1,10 @@
-#!/usr/bin/python3.8
-
-import marshal
-
-def print_hidden_names(file_path):
-    with open(file_path, 'rb') as pyc_file:
-        magic = pyc_file.read(4)  # Skip the magic number
-        timestamp = pyc_file.read(4)  # Skip the timestamp
-        code_object = marshal.load(pyc_file)
-
-        for const in code_object.co_consts:
-            if isinstance(const, str) and not const.startswith('__'):
-                print(const)
+#!/usr/bin/python3
 
 if __name__ == "__main__":
-    print_hidden_names('hidden_4.pyc')
+    import hidden_4
+
+    names = dir(hidden_4)
+    for name in names:
+        if name[:2] != "__":
+            print(name)
 
