f0656d8ed5874d56ddba912fea565d9491a596e6
Xage1 <kennethkiarie555@gmail.com>
2023-08-16 09:12:55 -0700

    New Project

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,10 +1,9 @@
+#!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new_matrix = []
-    
-    for row in matrix:
-        new_row = []
-        for num in row:
-            new_row.append(num ** 2)
-        new_matrix.append(new_row)
-    
-    return new_matrix
+    new = []
+    for i in matrix:
+        new2 = []
+        for j in i:
+            new2.append(j**2)
+        new.append(new2)
+    return new
