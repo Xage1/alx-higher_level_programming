595c93cb1ea3ee7f6dabd53f7a509c130266702c
Xage1 <kennethkiarie555@gmail.com>
2023-08-19 09:10:53 +0000

    Update

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-
+output = ""
 for tens in range(10):
     for ones in range(tens + 1, 10):
-        if tens < 9:
-            print("{:02d}, ".format(tens * 10 + ones), end="")
-        else:
-            print("{:02d}".format(tens * 10 + ones), end="")
+        output += "{:02d}, ".format(tens * 10 + ones)
+
+output = output[:-2]
+print(output)
