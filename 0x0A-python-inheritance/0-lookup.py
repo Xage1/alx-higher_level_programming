1f870a30e5fabb61a1e9b56c5e0010f39a1d9e66
Xage1 <Kennethkiarie555@gmail.com>
2023-10-03 03:22:32 -0700

    Update

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""
+This module provides a function to lookup and return a list of available attributes and methods of an object.
+"""
+
 def lookup(obj):
     """
     Returns a list of available attributes and methods of an object.
--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
+"""
+This module defines a custom list class, MyList.
+"""
+
 class MyList(list):
     def print_sorted(self):
-        """Print the list in ascending sorted order."""
+        """
+        Print the list in ascending sorted order.
+        """
         sorted_list = sorted(self)
         print(sorted_list)
