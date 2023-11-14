f136ad0b1d2f8afc738c1f4c29b31d1ea94774ad
Xage1 <kennethkiarie555@gmail.com>
2023-11-14 00:06:20 -0800

    Another ALX journey

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+const argsCount = process.argv.length;
+
+if (argsCount === 2)
+{
+	console.log("No argument");
+}
+else if (argsCount === 3)
+{
+	console.log("Argument found");
+}
+else
+{
+	console.log("Arguments found");
+}
